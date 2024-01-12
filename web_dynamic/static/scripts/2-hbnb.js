d0599b92e00c9b2869e3fb53c0a0a8884b893c4e
Sicelumusa <musaqwabe@gmail.com>
2024-01-12 15:18:13 -0800

    added task 4

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,23 @@
+$(function() {
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const names =Object.values(amenities);
+    if (names.length > 0) {
+      $('div.amenities > h4').text(names.join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
+
 // Wait for DOM content to load
 $(function() {
-  // Make a GET request to the API endpoin
+  // Make a GET request to the API endpoint
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     // Get the div#api_status element
     const apiStatusDiv = $('#api_status');
@@ -12,7 +29,7 @@ $(function() {
   } else {
     apiStatusDiv.removeClass('available');
   }
-  });
+  })
   .fail(function (error) {
     console.error('error fetching API status', error);
   });
